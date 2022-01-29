39d2677f21fc493e5894fc56084b1f67fd49404c
Goncalo-Palma-Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2022-01-29 19:44:04 +0000

    Added test for case where unmount should fail

---
+++ tests/client_server_unmount_fail.c
@@ -0,0 +1,25 @@
+#include "client/tecnicofs_client_api.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+/*  This test is similar to test1.c from the 1st exercise.
+    The main difference is that this one explores the
+    client-server architecture of the 2nd exercise. */
+
+int main(int argc, char **argv) {
+
+
+
+    if (argc < 3) {
+        printf("You must provide the following arguments: 'client_pipe_path "
+               "server_pipe_path'\n");
+        return 1;
+    }
+
+    assert(tfs_unmount() == -1);
+
+    printf("Successful test.\n");
+
+    return 0;
+}
